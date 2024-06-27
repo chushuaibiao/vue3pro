5a31878a01acd4b3620fe48ce305c0bb42bc3c54
chushuaibiao <2574950251@qq.com>
2024-06-27 09:55:24 +0800

    0627:vue3init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,394 @@
+<template>
+   <!-- <div class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div>
+
+  <div class="mb-4">
+    <el-button plain>Plain</el-button>
+    <el-button type="primary" plain>Primary</el-button>
+    <el-button type="success" plain>Success</el-button>
+    <el-button type="info" plain>Info</el-button>
+    <el-button type="warning" plain>Warning</el-button>
+    <el-button type="danger" plain>Danger</el-button>
+  </div>
+
+  <div class="mb-4">
+    <el-button round>Round</el-button>
+    <el-button type="primary" round>Primary</el-button>
+    <el-button type="success" round>Success</el-button>
+    <el-button type="info" round>Info</el-button>
+    <el-button type="warning" round>Warning</el-button>
+    <el-button type="danger" round>Danger</el-button>
+  </div>
+
+  <div>
+    <el-button :icon="Search" circle />
+    <el-button type="primary" :icon="Edit" circle />
+    <el-button type="success" :icon="Check" circle />
+    <el-button type="info" :icon="Message" circle />
+    <el-button type="warning" :icon="Star" circle />
+    <el-button type="danger" :icon="Delete" circle />
+  </div>
+
+
+  <div class="mb-4">
+    <el-button disabled>Default</el-button>
+    <el-button type="primary" disabled>Primary</el-button>
+    <el-button type="success" disabled>Success</el-button>
+    <el-button type="info" disabled>Info</el-button>
+    <el-button type="warning" disabled>Warning</el-button>
+    <el-button type="danger" disabled>Danger</el-button>
+  </div>
+
+  <div>
+    <el-button plain disabled>Plain</el-button>
+    <el-button type="primary" plain disabled>Primary</el-button>
+    <el-button type="success" plain disabled>Success</el-button>
+    <el-button type="info" plain disabled>Info</el-button>
+    <el-button type="warning" plain disabled>Warning</el-button>
+    <el-button type="danger" plain disabled>Danger</el-button>
+  </div>
+
+
+  <p>Basic link button</p>
+  <div class="mb-4">
+    <el-button
+      v-for="button in buttons"
+      :key="button.text"
+      :type="button.type"
+      link
+    >
+      {{ button.text }}
+    </el-button>
+  </div>
+
+  <p>Disabled link button</p>
+  <div>
+    <el-button
+      v-for="button in buttons"
+      :key="button.text"
+      :type="button.type"
+      link
+      disabled
+    >
+      {{ button.text }}
+    </el-button>
+  </div>
+
+  <p>Basic text button</p>
+  <div class="mb-4">
+    <el-button
+      v-for="button in buttons"
+      :key="button.text"
+      :type="button.type"
+      text
+    >
+      {{ button.text }}
+    </el-button>
+  </div>
+
+  <p>Background color always on</p>
+  <div class="mb-4">
+    <el-button
+      v-for="button in buttons"
+      :key="button.text"
+      :type="button.type"
+      text
+      bg
+    >
+      {{ button.text }}
+    </el-button>
+  </div>
+
+  <p>Disabled text button</p>
+  <div>
+    <el-button
+      v-for="button in buttons"
+      :key="button.text"
+      :type="button.type"
+      text
+      disabled
+    >
+      {{ button.text }}
+    </el-button>
+  </div>
+  <div>
+    <el-button type="primary" :icon="Edit" />
+    <el-button type="primary" :icon="Share" />
+    <el-button type="primary" :icon="Delete" />
+    <el-button type="primary" :icon="Search">Search</el-button>
+    <el-button type="primary">
+      Upload<el-icon class="el-icon--right"><Upload /></el-icon>
+    </el-button>
+  </div>
+  <el-button-group>
+    <el-button type="primary" :icon="ArrowLeft">Previous Page</el-button>
+    <el-button type="primary">
+      Next Page<el-icon class="el-icon--right"><ArrowRight /></el-icon>
+    </el-button>
+  </el-button-group>
+
+  <el-button-group class="ml-4">
+    <el-button type="primary" :icon="Edit" />
+    <el-button type="primary" :icon="Share" />
+    <el-button type="primary" :icon="Delete" />
+  </el-button-group>
+
+  <el-button type="primary" loading>Loading</el-button>
+  <el-button type="primary" :loading-icon="Eleme" loading>Loading</el-button>
+  <el-button type="primary" loading>
+    <template #loading>
+      <div class="custom-loading">
+        <svg class="circular" viewBox="-10, -10, 50, 50">
+          <path
+            class="path"
+            d="
+            M 30 15
+            L 28 17
+            M 25.61 25.61
+            A 15 15, 0, 0, 1, 15 30
+            A 15 15, 0, 1, 1, 27.99 7.5
+            L 15 15
+          "
+            style="stroke-width: 4px; fill: rgba(0, 0, 0, 0)"
+          />
+        </svg>
+      </div>
+    </template>
+    Loading
+  </el-button>
+
+  <div class="flex flex-wrap items-center mb-4">
+    <el-button size="large">Large</el-button>
+    <el-button>Default</el-button>
+    <el-button size="small">Small</el-button>
+    <el-button size="large" :icon="Search">Search</el-button>
+    <el-button :icon="Search">Search</el-button>
+    <el-button size="small" :icon="Search">Search</el-button>
+  </div>
+  <div class="flex flex-wrap items-center mb-4">
+    <el-button size="large" round>Large</el-button>
+    <el-button round>Default</el-button>
+    <el-button size="small" round>Small</el-button>
+    <el-button size="large" :icon="Search" round>Search</el-button>
+    <el-button :icon="Search" round>Search</el-button>
+    <el-button size="small" :icon="Search" round>Search</el-button>
+  </div>
+  <div class="flex flex-wrap items-center">
+    <el-button :icon="Search" size="large" circle />
+    <el-button :icon="Search" circle />
+    <el-button :icon="Search" size="small" circle />
+  </div>
+
+  <el-button>button</el-button>
+  <el-button tag="div" role="button" tabindex="0">div</el-button>
+  <el-button
+    type="primary"
+    tag="a"
+    href="https://github.com/element-plus/element-plus"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    a
+  </el-button>
+  <el-row :gutter="12" class="demo-radius">
+    <el-col
+      v-for="(radius, i) in radiusGroup"
+      :key="i"
+      :span="6"
+      :xs="{ span: 12 }"
+    >
+      <div class="title">{{ radius.name }}</div>
+      <div class="value">
+        <code>border-radius: {{ getValue(radius.type) || '0px' }}</code>
+      </div>
+      <div
+        class="radius"
+        :style="{
+          borderRadius: radius.type
+            ? `var(--el-border-radius-${radius.type})`
+            : '',
+        }"
+      />
+    </el-col>
+  </el-row>
+  <div class="flex justify-between items-center flex-wrap">
+    <div
+      v-for="(shadow, i) in shadowGroup"
+      :key="i"
+      class="flex flex-col justify-center items-center"
+      m="auto"
+      w="46"
+    >
+      <div
+        class="inline-flex"
+        h="30"
+        w="30"
+        m="2"
+        :style="{
+          boxShadow: `var(${getCssVarName(shadow.type)})`,
+        }"
+      />
+      <span p="y-4" class="demo-shadow-text" text="sm">
+        {{ shadow.name }}
+      </span>
+      <code text="xs">
+        {{ getCssVarName(shadow.type) }}
+      </code>
+    </div>
+  </div> -->
+  <div class ="common-layout">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </div>
+  <template>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+</template>
+<style>
+</style>
+<script lang="ts" setup>
+// import { Share, Upload } from '@element-plus/icons-vue'
+// import {
+//   ArrowLeft,
+//   ArrowRight,
+// } from '@element-plus/icons-vue'
+// import {
+//   Check,
+//   Delete,
+//   Edit,
+//   Message,
+//   Search,
+//   Star,
+// } from '@element-plus/icons-vue'
+// import { ref } from 'vue'
+// import { Eleme } from '@element-plus/icons-vue'
+// const buttons = [
+//   { type: '', text: 'plain' },
+//   { type: 'primary', text: 'primary' },
+//   { type: 'success', text: 'success' },
+//   { type: 'info', text: 'info' },
+//   { type: 'warning', text: 'warning' },
+//   { type: 'danger', text: 'danger' },
+// ] as const
+// const radiusGroup = ref([
+//   {
+//     name: 'No Radius',
+//     type: '',
+//   },
+//   {
+//     name: 'Small Radius',
+//     type: 'small',
+//   },
+//   {
+//     name: 'Large Radius',
+//     type: 'base',
+//   },
+//   {
+//     name: 'Round Radius',
+//     type: 'round',
+//   },
+// ])
+ 
+// const getValue = (type: string) => {
+//   const getCssVarValue = (prefix:any, type:any) =>
+//     getComputedStyle(document.documentElement).getPropertyValue(
+//       `--el-${prefix}-${type}`
+//     )
+//   return getCssVarValue('border-radius', type)
+// }
+
+// const shadowGroup = ref([
+//   {
+//     name: 'Basic Shadow',
+//     type: '',
+//   },
+//   {
+//     name: 'Light Shadow',
+//     type: 'light',
+//   },
+//   {
+//     name: 'Lighter Shadow',
+//     type: 'lighter',
+//   },
+//   {
+//     name: 'Dark Shadow',
+//     type: 'dark',
+//   },
+// ])
+
+// const getCssVarName = (type: string) => {
+//   return `--el-box-shadow${type ? '-' : ''}${type}`
+// }
+
+</script>
+
+<style scoped>
+/* .el-button .custom-loading .circular {
+  margin-right: 6px;
+  width: 18px;
+  height: 18px;
+  animation: loading-rotate 2s linear infinite;
+}
+.el-button .custom-loading .circular .path {
+  animation: loading-dash 1.5s ease-in-out infinite;
+  stroke-dasharray: 90, 150;
+  stroke-dashoffset: 0;
+  stroke-width: 2;
+  stroke: var(--el-button-text-color);
+  stroke-linecap: round;
+}
+.demo-radius .title {
+  color: var(--el-text-color-regular);
+  font-size: 18px;
+  margin: 10px 0;
+}
+.demo-radius .value {
+  color: var(--el-text-color-primary);
+  font-size: 16px;
+  margin: 10px 0;
+}
+.demo-radius .radius {
+  height: 40px;
+  width: 70%;
+  border: 1px solid var(--el-border-color);
+  border-radius: 0;
+  margin-top: 20px;
+}
+.demo-radius .title {
+  color: var(--el-text-color-regular);
+  font-size: 18px;
+  margin: 10px 0;
+}
+.demo-radius .value {
+  color: var(--el-text-color-primary);
+  font-size: 16px;
+  margin: 10px 0;
+}
+.demo-radius .radius {
+  height: 40px;
+  width: 70%;
+  border: 1px solid var(--el-border-color);
+  border-radius: 0;
+  margin-top: 20px;
+}  */
+
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const buttons = [
+  { type: '', text: 'plain' },
+  { type: 'primary', text: 'primary' },
+  { type: 'success', text: 'success' },
+  { type: 'info', text: 'info' },
+  { type: 'warning', text: 'warning' },
+  { type: 'danger', text: 'danger' },
+] as const
+</script>
+
+<template>
+   <div class="mb-4">
+    <el-button
+      v-for="button in buttons"
+      :key="button.text"
+      :type="button.type"
+      link
+    >
+      {{ button.text }}
+    </el-button>
+    <div></div>
+    <el-button
+      v-for="button in buttons"
+      :key="button.text"
+      :type="button.type"
+      link
+      disabled
+    >
+      {{ button.text }}
+    </el-button>
+  </div>
+</template>
